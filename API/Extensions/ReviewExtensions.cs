dbafd057a6801e1bb35778338cd935a96ba777ed
Mochi <antivirused29112003@gmail.com>
2024-06-10 12:41:03 +0700

    Add Pre Order API

---
+++ API/Extensions/ReviewExtensions.cs
@@ -0,0 +1,22 @@
+﻿using API.Entities;
+
+namespace API.Extensions
+{
+    public static class ReviewExtensions
+    {
+        public static IQueryable<Review> FilterBy(this IQueryable<Review> querry, string? orderBy)
+        {
+            if (string.IsNullOrEmpty(orderBy)) return querry;
+
+            querry = orderBy switch
+            {
+                "rate" => querry.OrderBy(p => p.Rating),
+                "rateDesc" => querry.OrderByDescending(p => p.Rating),
+                "product" => querry.OrderBy(p => p.Product),
+                _ => querry.OrderBy(p => p.User),
+            };
+
+            return querry;
+        }
+    }
+}
